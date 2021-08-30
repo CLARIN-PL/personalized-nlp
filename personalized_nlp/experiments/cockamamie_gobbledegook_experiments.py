7716cc6b5e5e43b8d6320606bfe785e1cee63220
JuBi <b.julita@asko-atv.pl>
2021-08-31 01:10:20 +0200

    Modifying files to scan subset c

--- personalized_nlp/experiments/cockamamie_gobbledegook_experiments.py
+++ personalized_nlp/experiments/cockamamie_gobbledegook_experiments.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
                     }
 
                     logger = pl_loggers.WandbLogger(
-                        save_dir=LOGS_DIR, config=hparams, project='cockamamie-b',
+                        save_dir=LOGS_DIR, config=hparams, project='cockamamie-c',
                         entity='persemo', log_model=False)
 
                     output_dim = len(data_module.class_dims)
