74230d514d856f2751d2ca6162752f388db1d6c2
Satyrr <gruzi12@gmail.com>
2021-09-20 23:44:08 +0200

    Add new metrics

--- personalized_nlp/utils/metrics.py
+++ personalized_nlp/utils/metrics.py
@@ -3,26 +3,32 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, classification_report, r2_score
 from collections import defaultdict
+import torchmetrics
+from torch import Tensor
+
 
 def evaluate_classification(test_predictions, true_labels, class_dims):
     dims_results = {}
     for cls_idx in range(len(class_dims)):
-        start_idx =  sum(class_dims[:cls_idx])
-        end_idx =  start_idx + class_dims[cls_idx]
+        start_idx = sum(class_dims[:cls_idx])
+        end_idx = start_idx + class_dims[cls_idx]
         preds = np.argmax(test_predictions[:, start_idx:end_idx], axis=1)
-        
+
         if true_labels.ndim > 1:
             targets = true_labels[:, cls_idx]
         else:
             targets = true_labels
 
-        dims_results[cls_idx] = classification_report(targets, preds, output_dict=True)
-    
+        dims_results[cls_idx] = classification_report(
+            targets, preds, output_dict=True)
+
     return dims_results
+
 
 def evaluate_regression(test_predictions, true_labels):
     if true_labels.ndim > 1:
-        losses = [r2_score(true_labels[:, i], test_predictions[:, i]) for i in range(test_predictions.shape[1])]
+        losses = [r2_score(true_labels[:, i], test_predictions[:, i])
+                  for i in range(test_predictions.shape[1])]
     else:
         losses = [r2_score(true_labels, test_predictions)]
 
@@ -35,10 +41,10 @@ def get_result_dataframe(results, regression=False, class_names=None):
     for experiment in results:
         hparams = experiment[0]
         metrics = experiment[1]
-        
+
         for hparam_key, hparam_value in hparams.items():
             result_dict[hparam_key].append(hparam_value)
-        
+
         if regression:
             for class_idx, r_squared in enumerate(metrics['r_squared']):
                 if class_names is not None:
@@ -46,7 +52,7 @@ def get_result_dataframe(results, regression=False, class_names=None):
                 else:
                     class_name = str(class_idx)
 
-                result_dict['r_squared_' + class_name].append(r_squared) 
+                result_dict['r_squared_' + class_name].append(r_squared)
         else:
             for class_idx, values in metrics.items():
                 if not isinstance(class_idx, int):
@@ -58,7 +64,7 @@ def get_result_dataframe(results, regression=False, class_names=None):
                     class_name = str(class_idx)
 
                 f1_score = values['1']['f1-score']
-                macro_f1_score = values['macro avg']['f1-score']    
+                macro_f1_score = values['macro avg']['f1-score']
                 accuracy = values['accuracy']
                 result_dict['f1_score_' + class_name].append(f1_score)
                 result_dict['macro_f1_' + class_name].append(macro_f1_score)
@@ -71,3 +77,27 @@ def get_result_dataframe(results, regression=False, class_names=None):
         result_dict['testing_time'].append(testing_time)
 
     return pd.DataFrame(result_dict)
+
+
+class F1Class(torchmetrics.F1):
+
+    def compute(self) -> Tensor:
+        f1_for_classes = super().compute()
+
+        return f1_for_classes[-1]
+
+
+class PrecisionClass(torchmetrics.Precision):
+
+    def compute(self) -> Tensor:
+        precision_for_classes = super().compute()
+
+        return precision_for_classes[-1]
+
+
+class RecallClass(torchmetrics.Recall):
+
+    def compute(self) -> Tensor:
+        recall_for_classes = super().compute()
+
+        return recall_for_classes[-1]
