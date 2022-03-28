59916cf259f178fcf5ce1f5cd8a0b3c8230d6551
artikandri <argikartika@gmail.com>
2022-03-28 13:37:45 +0200

    feat: added NetTuned to models/__init__

--- personalized_nlp/models/__init__.py
+++ personalized_nlp/models/__init__.py
@@ -1,4 +1,3 @@
-
 from personalized_nlp.models.baseline import Net
 from personalized_nlp.models.onehot import NetOneHot
 from personalized_nlp.models.human_bias import HumanBiasNet
@@ -6,13 +5,15 @@ from personalized_nlp.models.bias import AnnotatorBiasNet
 from personalized_nlp.models.word_bias import WordBiasNet
 from personalized_nlp.models.annotator import AnnotatorEmbeddingNet
 from personalized_nlp.models.annotator_word import AnnotatorWordEmbeddingNet
+from personalized_nlp.models.baseline_tuned import NetTuned
 
 models = {
     'baseline': Net,
-    'onehot': NetOneHot, 
-    'peb': HumanBiasNet, 
+    'baseline_tuned': NetTuned,
+    'onehot': NetOneHot,
+    'peb': HumanBiasNet,
     'word_bias': WordBiasNet,
-    'bias': AnnotatorBiasNet, 
-    'embedding': AnnotatorEmbeddingNet, 
+    'bias': AnnotatorBiasNet,
+    'embedding': AnnotatorEmbeddingNet,
     'word_embedding': AnnotatorWordEmbeddingNet
 }
