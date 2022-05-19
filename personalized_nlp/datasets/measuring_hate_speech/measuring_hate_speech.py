5edcf1fca1ee51cc2a2271f29caaca5745fed7fc
Kamil Kanclerz <k.kanclerz4@gmail.com>
2022-05-19 13:54:07 +0200

    Add Measuring Hate Speech dataset to DVC and as a DataModule

---
+++ personalized_nlp/datasets/measuring_hate_speech/measuring_hate_speech.py
@@ -0,0 +1,55 @@
+import os
+
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+from personalized_nlp.settings import STORAGE_DIR
+from personalized_nlp.utils.data_splitting import split_texts
+from personalized_nlp.datasets.datamodule_base import BaseDataModule
+
+
+class MeasuringHateSpeechDataModule(BaseDataModule):
+
+    @property
+    def data_dir(self) -> Path:
+        return STORAGE_DIR / "measuring_hate_speech"
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        os.makedirs(self.data_dir / "embeddings", exist_ok=True)
+
+    def prepare_data(self) -> None:
+        self.data = pd.read_csv(
+            self.data_dir / "data.tsv",
+            sep="\t",
+        )
+
+        self.annotators = pd.read_csv(
+            self.data_dir / "worker_demographics.tsv",
+            sep="\t",
+        )
+
+        self.annotations = pd.read_csv(self.data_dir / "annotations.tsv",
+                                       sep="\t")
+
+    @property
+    def class_dims(self) -> List[int]:
+        return [5, 5, 5, 5, 5, 5, 5, 5, 5, 3]
+
+    @property
+    def annotation_columns(self) -> List[str]:
+        return [
+            "sentiment", "respect", "insult", "humiliate", "status",
+            "dehumanize", "violence", "genocide", "attack_defend", "hatespeech"
+        ]
+
+    @property
+    def embeddings_path(self) -> Path:
+        return (self.data_dir /
+                f"embeddings/rev_id_to_emb_{self.embeddings_type}.p")
