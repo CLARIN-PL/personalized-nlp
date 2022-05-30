372391483dd186680519b4b4d8bdab43f23b6de4
artikandri <argikartika@gmail.com>
2022-05-30 15:50:40 +0200

    for running

--- personalized_nlp/experiments/goemo_single_ann_tune.py
+++ personalized_nlp/experiments/goemo_single_ann_tune.py
@@ -24,7 +24,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 os.environ["WANDB_START_METHOD"] = "thread"
 
 if __name__ == "__main__":
-    regression = True
+    regression = False
     datamodule_cls = GoEmotionsDataModule
     embedding_types = ['roberta']
     model_types = ['baseline']
