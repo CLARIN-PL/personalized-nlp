065ed8cb1e09f2dea4e0cb430b9ca05b3bfa2390
Kamil Kanclerz <k.kanclerz4@gmail.com>
2022-08-05 18:28:14 +0200

    Add measures regarding balanced user and text votes

--- active_learning/algorithms/__init__.py
+++ active_learning/algorithms/__init__.py
@@ -1,11 +1,20 @@
 from active_learning.algorithms.base import TextSelectorBase
 from active_learning.algorithms.random import RandomSelector
-from active_learning.algorithms.confidence import ConfidenceSelector
+from active_learning.algorithms.confidence import (
+    ConfidenceSelector, )
 from active_learning.algorithms.avg_confidence_per_user import (
-    AverageConfidencePerUserSelector,
-)
+    AverageConfidencePerUserSelector, )
 from active_learning.algorithms.annotation_diversity import (
-    TextAnnotationDiversitySelector,
-)
-from active_learning.algorithms.max_positive import MaxPositiveClassSelector
-from active_learning.algorithms.confidence_all_dims import ConfidenceAllDimsSelector
+    TextAnnotationDiversitySelector, )
+from active_learning.algorithms.scaled_annotation_diversity import (
+    TextScaledAnnotationDiversitySelector, )
+from active_learning.algorithms.max_positive import (
+    MaxPositiveClassSelector, )
+from active_learning.algorithms.confidence_all_dims import (
+    ConfidenceAllDimsSelector, )
+from active_learning.algorithms.balanced_confidence import (
+    BalancedConfidenceSelector, )
+from active_learning.algorithms.balanced_classes_per_text import (
+    BalancedClassesPerTextSelector, )
+from active_learning.algorithms.balanced_classes_per_user import (
+    BalancedClassesPerUserSelector, )
---
+++ active_learning/algorithms/balanced_confidence.py
@@ -0,0 +1,66 @@
+import warnings
+
+import pandas as pd
+import numpy as np
+
+from active_learning.algorithms.base import TextSelectorBase
+
+
+class BalancedConfidenceSelector(TextSelectorBase):
+
+    def __init__(self,
+                 select_minimal_texts: bool = True,
+                 *args,
+                 **kwargs) -> None:
+        """Selector based on model confidence balance between classes. 
+                If there is a conflict, it is resolved based on absolute difference between model confidences between classes for a specific task.
+                If there are more than one task then the average is calculated across all tasks
+
+                """
+        super(BalancedConfidenceSelector, self).__init__()
+        self.select_minimal_texts: bool = select_minimal_texts
+
+    def select_annotations(
+        self,
+        texts: pd.DataFrame,
+        amount: int,
+        annotated: pd.DataFrame,
+        not_annotated: pd.DataFrame,
+        confidences: np.ndarray,
+    ):
+        """Select annotations using rule, described in __init__()
+
+        Args:
+            texts (pd.DataFrame): Dataframe with texts data. It contains (at least)
+            two columns: `text_id` (int) and `text` (str)
+            amount (int): Number of annotations to select from `not_annotated` dataframe.
+            annotated (pd.DataFrame): Already selected annotations. It is a Dataframe with
+            (at least) two columns: `text_id` and `annotator_id`.
+            not_annotated (pd.DataFrame): Annotations to select. It is a Dataframe with
+            (at least) two columns: `text_id` and `annotator_id`. The algorithm should return
+            `amount` of rows from this dataframe.
+            confidences (np.ndarray): Numpy array of prediction confidences for all annotations
+            in `not_annotated` dataframe. It has length equal to length of `not_annotated` dataframe.
+
+        Returns:
+            pd.DataFrame: Dataframe with subset of rows of `not_annotated` dataframe with length equal to
+            `amount`.
+        """
+        if confidences is not None:
+            dims = confidences.shape[1]
+            print(f'INFO: confidences shape: {confidences.shape}')
+            confidences = confidences.reshape(-1, int(dims / 2), 2)
+            confidences = np.absolute(np.diff(confidences, axis=2))
+            confidences = confidences.mean(axis=1)
+            confidences = confidences.flatten()
+
+            sorted_index = np.argsort(confidences)
+
+            return not_annotated.iloc[sorted_index[:amount]]
+
+        warnings.warn(
+            f"There is no confidences, sampled {amount} of samples from not annotated data."
+        )
+
+        amount = min(amount, len(not_annotated.index))
+        return not_annotated.sample(n=amount)
