5d0dfd9ddd15b89deab2e70605ade516aad90c12
Satyrr <gruzi12@gmail.com>
2022-09-21 22:43:11 +0200

    Update metrics

--- active_learning/algorithms/reinforce.py
+++ active_learning/algorithms/reinforce.py
@@ -1,3 +1,4 @@
+from ast import Not
 from typing import List, Optional
 import pandas as pd
 import numpy as np
@@ -10,9 +11,11 @@ class ReinforceSelector(TextSelectorBase):
         class_dims: Optional[List[int]] = None,
         annotation_columns: Optional[List[str]] = None,
         amount_per_user: Optional[int] = None,
+        text_embeddings: Optional[np.ndarray] = None,
     ) -> None:
         super().__init__(class_dims, annotation_columns, amount_per_user)
         self.regressor = None
+        self.text_embeddings = text_embeddings
 
     def get_metrics(
         self,
@@ -21,14 +24,39 @@ class ReinforceSelector(TextSelectorBase):
         not_annotated: pd.DataFrame,
         confidences: Optional[np.ndarray] = None,
     ):
+        cols = ["text_count", "annotator_count", "max_confidence"]
+        if confidences is not None:
+            cols += [f"conf_{i}" for i in range(confidences.shape[1])]
+
         text_counts_df = annotated.text_id.value_counts().reset_index()
         text_counts_df.columns = ["text_id", "text_count"]
 
-        sampled_annotations = not_annotated.merge(text_counts_df, how="left").fillna(
-            100
-        )
+        annotator_counts_df = annotated.annotator_id.value_counts().reset_index()
+        annotator_counts_df.columns = ["annotator_id", "annotator_count"]
 
-        return sampled_annotations["text_count"].values[-1, None]
+        not_annotated = not_annotated.merge(text_counts_df, how="left").fillna(0)
+        not_annotated = not_annotated.merge(annotator_counts_df, how="left").fillna(0)
+
+        if confidences is not None:
+            not_annotated["max_confidence"] = confidences.argmax(axis=1)
+            for i in range(confidences.shape[1]):
+                not_annotated[f"conf_{i}"] = confidences[:, i]
+        else:
+            not_annotated["max_confidence"] = 0
+
+        metrics = not_annotated.loc[:, cols].values
+        if self.text_embeddings is not None:
+            text_id_to_idx = {id: idx for id, idx in zip(texts.index, texts.text_id)}
+            embeddings = np.vstack(
+                [self.text_embeddings[text_id_to_idx[i]] for i in not_annotated.text_id]
+            )
+            metrics = np.hstack([metrics, embeddings])
+
+        num_done_annotations = len(annotated.index)
+        num_done_annotations_arr = np.ones((metrics.shape[0], 1)) * num_done_annotations
+        metrics = np.hstack([metrics, num_done_annotations_arr])
+
+        return metrics
 
     def sort_annotations(
         self,
