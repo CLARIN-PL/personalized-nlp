b1786735e6633c0471a1442809250a3a8ec4059b
kaszycaO <o_kaszyca@interia.pl>
2022-11-11 16:50:39 +0100

    Initial
    + AL experiments are now runnable
    + Add tests folder
    + Repair some of AL datasets configurations

---
+++ tests/test_al_algorithms.py
@@ -0,0 +1,15 @@
+import active_learning.algorithms as a
+import pytest
+
+
+@pytest.fixture
+def init_params(scope="session"):
+    pass
+
+@pytest.fixture
+def get_dataset(scope="session"):
+    pass
+
+@pytest.fixture
+def get_additional_data(scope="session"):
+    pass
