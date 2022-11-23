5ff65848fc3622cea90101420e6e13a84b842754
Igor Cichecki <cicheckiigor@gmail.com>
2022-11-24 00:00:56 +0100

    feat: introduce base for active learning flows package - mostly copy paste from active learning module

--- personalized_active_learning/active_learning_flows/__init__.py
+++ personalized_active_learning/active_learning_flows/__init__.py
@@ -3,3 +3,5 @@
 Currently each modification of AL flow (e.g. pretraining) requires dedicated class.
 
 """
+from .base import ActiveLearningFlowBase
+from .definitions import StandardActiveLearningFlow
