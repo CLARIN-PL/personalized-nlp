d48849631a0331953228d7f3cd65b7ac1cd7ad0b
Dominika <dszydlo28@gmail.com>
2022-12-02 12:36:34 +0100

    personal metrics refactor

--- personalized_active_learning/metrics/personal_metrics.py
+++ personalized_active_learning/metrics/personal_metrics.py
@@ -1,63 +1,143 @@
-# TODO: REFACTOR!!!
 from collections import defaultdict
-from typing import Any
+from typing import Any, Dict, List
 
 import numpy as np
+import pytorch_lightning as pl
 import torch
+
 from pytorch_lightning.callbacks import Callback
 from sklearn.metrics import classification_report
-import pytorch_lightning as pl
 
 
 class PersonalizedMetricsCallback(Callback):
+    """Class responsible for computing personalized metrics."""    
     def __init__(self):
         super().__init__()
-        self._test_outputs: list[dict[str, Any]] = []
-        self._valid_outputs: list[dict[str, Any]] = []
+        self._test_outputs: List[Dict[str, Any]] = []
+        self._valid_outputs: List[Dict[str, Any]] = []
+
 
     def on_validation_batch_end(
-        self,
+        self: "PersonalizedMetricsCallback",
         trainer: "pl.Trainer",
         pl_module: "pl.LightningModule",
-        outputs,
+        outputs: Dict[str, Any],
         batch: Any,
         batch_idx: int,
         dataloader_idx: int,
     ) -> None:
+        """Gather model's outputs for validation batch.
+
+        Args:
+            trainer (pl.Trainer): Model trainer.
+            pl_module (pl.LightningModule): Trained model.
+            outputs (Dict[str, Any]): Model outputs for current validation batch.
+            batch (Any): Current validation batch.
+            batch_idx (int): Current validation batch index.
+            dataloader_idx (int): Dataloader index.
+        """        
         self._valid_outputs.append(outputs)
 
+
     def on_validation_epoch_end(
-        self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+        self: "PersonalizedMetricsCallback",
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule"
     ) -> None:
+        """Compute personalized metrics at the end of a validation epoch.
+
+        Args:
+            trainer (pl.Trainer): Model trainer.
+            pl_module (pl.LightningModule): Trained model.
+        """        
         annotator_ids = torch.cat(
             [o["x"].annotator_ids for o in self._valid_outputs], dim=0
         )
+
         y_pred = torch.cat([o["y_pred"] for o in self._valid_outputs], dim=0)
         y_true = torch.cat([o["y"] for o in self._valid_outputs], dim=0)
 
-        self.log_all_metrics(annotator_ids, y_pred, y_true, pl_module, "valid")
+        self.log_all_metrics(
+            annotator_ids=annotator_ids,
+            y_pred=y_pred,
+            y_true=y_true,
+            pl_module=pl_module,
+            split="valid"
+        )
 
-        self._valid_outputs: list[dict[str, Any]] = []
+        self._valid_outputs: List[Dict[str, Any]] = []
 
-    def on_test_batch_end(self, trainer, pl_module, outputs, *args, **kwargs):
+
+    def on_test_batch_end(
+        self: "PersonalizedMetricsCallback",
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        outputs: Dict[str, Any],
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """Gather model outputs for test batch.
+
+        Args:
+            trainer (pl.Trainer): Model trainer.
+            pl_module (pl.LightningModule): Trained model.
+            outputs (Dict[str, Any]): Model ouputs for current test batch.
+        """        
         self._test_outputs.append(outputs)
 
-    def on_test_epoch_end(self, trainer, pl_module, *args, **kwargs):
+
+    def on_test_epoch_end(
+        self: "PersonalizedMetricsCallback",
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        *args: Any,
+        **kwargs: Any
+        ) -> None:
+        """Compute personalized metrics at the end of a test epoch.
+
+        Args:
+            trainer (pl.Trainer): Model trainer.
+            pl_module (pl.LightningModule): Trained model.
+        """        
         annotator_ids = torch.cat(
             [o["x"].annotator_ids for o in self._test_outputs], dim=0
         )
+
         y_pred = torch.cat([o["y_pred"] for o in self._test_outputs], dim=0)
         y_true = torch.cat([o["y"] for o in self._test_outputs], dim=0)
 
-        self.log_all_metrics(annotator_ids, y_pred, y_true, pl_module, "test")
+        self.log_all_metrics(
+            annotator_ids=annotator_ids,
+            y_pred=y_pred,
+            y_true=y_true,
+            pl_module=pl_module,
+            split="test",
+        )
 
-        self._test_outputs: list[dict[str, Any]] = []
+        self._test_outputs: List[Dict[str, Any]] = []
 
-    def log_all_metrics(self, annotator_ids, output, y, pl_module, split: str):
+
+    def log_all_metrics(
+        self: "PersonalizedMetricsCallback",
+        annotator_ids: "torch.Tensor",
+        y_pred: "torch.Tensor",
+        y_true: "torch.Tensor",
+        pl_module: "pl.LightningModule",
+        split: str
+    ) -> None:
+        """Log all personalized metrics for model.
+
+        Args:
+            annotator_ids (torch.Tensor): Tensor with annotator ids.
+            y_pred (torch.Tensor): Model predictions.
+            y_true (torch.Tensor): Ground truth.
+            pl_module (pl.LightningModule): Trained model.
+            split (str): Type of split.
+        """        
         class_dims = pl_module.class_dims
         class_names = pl_module.class_names
 
-        output = torch.softmax(output, dim=1)
+        y_pred = torch.softmax(y_pred, dim=1)
 
         metrics = defaultdict(list)
 
@@ -67,17 +147,17 @@ class PersonalizedMetricsCallback(Callback):
                 end_idx = start_idx + class_dims[cls_dim_idx]
 
                 person_confidences = (
-                    output[annotator_ids == annotator_id, start_idx:end_idx].cpu().numpy()
+                    y_pred[annotator_ids == annotator_id, start_idx:end_idx].cpu().numpy()
                 )
+
                 person_y_pred = np.argmax(person_confidences, axis=1)
                 person_y_true = (
-                    y[annotator_ids == annotator_id, cls_dim_idx].long().cpu().numpy()
+                    y_true[annotator_ids == annotator_id, cls_dim_idx].long().cpu().numpy()
                 )
 
-
                 personal_metrics = classification_report(
-                    person_y_true,
-                    person_y_pred,
+                    y_true=person_y_true,
+                    y_pred=person_y_pred,
                     output_dict=True,
                     zero_division=0,  # We don't our output to be filled with warnings
                 )
@@ -96,4 +176,4 @@ class PersonalizedMetricsCallback(Callback):
 
         for metric_key in metrics:
             metric_values = metrics.get(metric_key)
-            pl_module.log(metric_key, np.mean(metric_values))
+            pl_module.log(name=metric_key, value=np.mean(metric_values))
