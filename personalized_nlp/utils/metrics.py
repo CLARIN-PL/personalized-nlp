702a7285bf100d5f24039a969c3dc07b5636a1c8
Satyrr <gruzi12@gmail.com>
2023-01-20 02:15:46 +0100

    Fix class metrics

--- personalized_nlp/utils/metrics.py
+++ personalized_nlp/utils/metrics.py
@@ -1,38 +1,39 @@
+from typing import Any
 
 import torchmetrics
 from torch import Tensor
 
-class F1Class(torchmetrics.F1):
-    
-    def __init__(self, class_idx=-1, *args, **kwargs) -> None:
+
+class F1Class(torchmetrics.classification.MulticlassF1Score):
+    def __init__(self, class_idx: int = -1, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
         self.class_idx = class_idx
-        
+
     def compute(self) -> Tensor:
         f1_for_classes = super().compute()
 
         return f1_for_classes[self.class_idx]
 
 
-class PrecisionClass(torchmetrics.Precision):
-    def __init__(self, class_idx=-1, *args, **kwargs) -> None:
+class PrecisionClass(torchmetrics.classification.MulticlassPrecision):
+    def __init__(self, class_idx: int = -1, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        
+
         self.class_idx = class_idx
-        
+
     def compute(self) -> Tensor:
         precision_for_classes = super().compute()
 
         return precision_for_classes[self.class_idx]
 
 
-class RecallClass(torchmetrics.Recall):
-    def __init__(self, class_idx=-1, *args, **kwargs) -> None:
+class RecallClass(torchmetrics.classification.MulticlassRecall):
+    def __init__(self, class_idx: int = -1, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        
+
         self.class_idx = class_idx
-        
+
     def compute(self) -> Tensor:
         recall_for_classes = super().compute()
 
