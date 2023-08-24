3e8ba77d3bb65eae2568674b144c9d52aa1f787a
Satyrr <gruzi12@gmail.com>
2023-08-25 00:17:06 +0200

    Add weighted conformity

--- personalized_nlp/utils/controversy.py
+++ personalized_nlp/utils/controversy.py
@@ -15,6 +15,15 @@ def get_text_controversy(annotations: pd.DataFrame, score_column: str) -> pd.Dat
 
 
 def get_conformity(annotations: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
+    g_conformity = get_general_conformity(annotations, columns)
+    w_conformity = get_weighted_conformity(annotations, columns)
+
+    return pd.concat([g_conformity, w_conformity], axis=1)
+
+
+def get_general_conformity(
+    annotations: pd.DataFrame, columns: List[str]
+) -> pd.DataFrame:
     """Computes conformity for each annotator. Works only for binary classification problems."""
     df = annotations.copy()
 
@@ -53,6 +62,53 @@ def get_conformity(annotations: pd.DataFrame, columns: List[str]) -> pd.DataFram
 
         conformity_df = col_conformity_df.merge(pos_col_conformity_df, how="left")
         conformity_df = conformity_df.merge(neg_col_conformity_df, how="left")
-        conformity_dfs.append(conformity_df.set_index('annotator_id'))
+        conformity_dfs.append(conformity_df.set_index("annotator_id"))
 
-    return pd.concat(conformity_dfs, axis=1)
+    return pd.concat(conformity_dfs, axis=1).fillna(1.0)
+
+
+def get_weighted_conformity(
+    annotations: pd.DataFrame, columns: List[str]
+) -> pd.DataFrame:
+    """Computes conformity for each annotator. Works only for binary classification problems."""
+    df = annotations.copy()
+
+    mean_scores = df.groupby("text_id")[columns].mean()
+    mean_scores = mean_scores.rename(columns=lambda col: col + "_mean")
+    df = df.merge(mean_scores.reset_index())
+
+    conformity_dfs = []
+    for col in columns:
+        df[f"{col}_annotator_agreement"] = df[f"{col}_mean"]
+        df.loc[df[col] == 0, f"{col}_annotator_agreement"] = (
+            1.0 - df[f"{col}_annotator_agreement"]
+        )
+
+        positive_df = df[df[col] == 1]
+        negative_df = df[df[col] == 0]
+
+        col_conformity_df = (
+            df.groupby(["annotator_id"])[[f"{col}_annotator_agreement"]]
+            .mean()
+            .rename(columns=lambda _: col + "_w_conformity")
+            .reset_index()
+        )
+
+        pos_col_conformity_df = (
+            positive_df.groupby(["annotator_id"])[[f"{col}_annotator_agreement"]]
+            .mean()
+            .rename(columns=lambda _: col + "_pos_w_conformity")
+            .reset_index()
+        )
+        neg_col_conformity_df = (
+            negative_df.groupby(["annotator_id"])[[f"{col}_annotator_agreement"]]
+            .mean()
+            .rename(columns=lambda _: col + "_neg_w_conformity")
+            .reset_index()
+        )
+
+        conformity_df = col_conformity_df.merge(pos_col_conformity_df, how="left")
+        conformity_df = conformity_df.merge(neg_col_conformity_df, how="left")
+        conformity_dfs.append(conformity_df.set_index("annotator_id"))
+
+    return pd.concat(conformity_dfs, axis=1).fillna(1.0)
