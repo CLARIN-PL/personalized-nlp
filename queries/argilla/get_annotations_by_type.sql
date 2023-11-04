8c2755a38e1dfe6cfb91594758a4a50dd007d198
Kamil Kanclerz <32719423+Netherwulf@users.noreply.github.com>
2023-11-04 17:29:04 +0100

    Update Argilla query for getting annotations by type

--- queries/argilla/get_annotations_by_type.sql
+++ queries/argilla/get_annotations_by_type.sql
@@ -1,2 +1,16 @@
 SELECT *
-FROM users
+FROM responses JOIN
+    (SELECT records.id
+    FROM
+        (SELECT *
+          FROM datasets
+          WHERE name LIKE '%Correct_LLM_Answer%') ds
+            JOIN
+            records
+            ON records.dataset_id = ds.id) rec_ds
+    ON responses.record_id = rec_ds.id
+
+-- %Task_Rank%
+-- %Classify_Answer%
+-- %Correct_LLM_Answer%
+-- %Create_Answer%
